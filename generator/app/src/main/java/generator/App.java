80093ef1b3fffee0bb0b6e3d63ffacee43e6c4ab
Francisco Caldeira <francisco.caldeira@outlook.com>
2023-06-02 02:23:59 +0100

    working kafka producer poc

--- generator/app/src/main/java/generator/App.java
+++ generator/app/src/main/java/generator/App.java
@@ -4,17 +4,44 @@
 package generator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import eventgenerator.EventGeneratorImpl;
+import kafka.KafkaMessageHandler;
+import kafka.KafkaMessageHandlerImpl;
+import kafka.MessageHelper;
+import kafka.Producer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.json.simple.JSONObject;
 import structures.InitEvent;
 
-import java.io.File;
 import java.io.IOException;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+    private static class ApplicationMessageHandlerImpl implements KafkaMessageHandler {
+
+        /**
+         * The Log.
+         */
+        //static Logger log = Logger.getLogger(ApplicationMessageHandlerImpl.class.getName());
+
+        @Override
+        public void processMessage(String topicName, ConsumerRecord<String, String> message) throws Exception {
+            String source = KafkaMessageHandlerImpl.class.getName();
+            JSONObject obj = MessageHelper.getMessageLogEntryJSON(source, topicName,message.key(),message.value());
+            System.out.println(obj.toJSONString());
+            //log.info(obj.toJSONString());
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println(new App().getGreeting());
+        EventGeneratorImpl eventGenerator = new EventGeneratorImpl();
+        InitEvent initEvent = eventGenerator.generateInitEvent("2023-06-01");
+        ObjectMapper mapper = new ObjectMapper();
+        String initEventString = mapper.writeValueAsString(initEvent);
+        System.out.println(initEventString);
+        new Producer().runAlways("magic-topic", new ApplicationMessageHandlerImpl());
     }
 
-    public static void main(String[] args) throws IOException {
-        System.out.println(new App().getGreeting());
+    public String getGreeting() {
+        return "Hello World!";
     }
 }
