9cc93d0aac94de97ecbf9b510c1b4ce54e4858d4
Francisco Caldeira <francisco.caldeira@outlook.com>
2023-06-10 18:57:30 +0100

    bronze init dag stream

---
+++ airflow/dags/bronze/launch_stream_consumer_dag.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from spark_task_helper import get_spark_tasks
+from bronze.config.config import config_dict
+
+
+"""
+launches streaming consumer 
+"""
+with DAG(
+    "bronze_extraction_dag",
+    schedule=None,
+    start_date=datetime(2023, 6, 2),
+    tags=["bronze", "kafka", "consumer"],
+) as dag:
+    tasks = []
+    for pipeline in config_dict["spark"]:
+        tasks.append(
+            BashOperator(
+                task_id=pipeline,
+                bash_command=get_spark_tasks(config_dict["spark"][pipeline])
+            )
+        )
