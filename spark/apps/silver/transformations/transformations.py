9918884a0db68acbf409f3bfc8ef42233683dd1f
Francisco Caldeira <francisco.caldeira@outlook.com>
2023-06-12 01:51:14 +0100

    tweaks silver

--- spark/apps/silver/transformations/transformations.py
+++ spark/apps/silver/transformations/transformations.py
@@ -1,4 +1,4 @@
-from pyspark.sql.functions import col, upper, udf
+from pyspark.sql.functions import col, upper, udf, from_unixtime
 import pycountry
 
 
@@ -12,3 +12,6 @@ def get_country_name_from_country_code2(df, country_col):
     df = df.drop(country_col)
     return df
 
+def unixtimestamp_to_date(df, unix_ts_col, date_col):
+    df = df.withColumn(date_col, from_unixtime(col(unix_ts_col)/1000, format="yyyy-MM-dd"))
+    return df
