d0aa721ff1830575e569a4c7a466d134c8ea82df
Francisco Caldeira <francisco.caldeira@outlook.com>
2023-06-12 01:51:24 +0100

    adding airflow

---
+++ airflow/dags/gold/launch_stream_gold_dag.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from spark_task_helper import get_spark_tasks
+from silver.config.config import config_dict
+
+
+"""
+launches streaming consumer 
+"""
+
+
+def create_dag(name, config):
+    with DAG(
+        f"gold_agreggation_dag_{name}",
+        schedule=None,
+        start_date=datetime(2023, 6, 2),
+        tags=["silver", "s3", "transformation"],
+    ) as dag:
+        BashOperator(task_id=name, bash_command=get_spark_tasks(config["spark"]))
+
+
+for dag in config_dict["dags"]:
+    create_dag(dag, config_dict["dags"][dag])
